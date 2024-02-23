c7b4969a1a8838b215e07610142bc83755fc4edd
Andy Dalaimo <adalaimo@gmail.com>
2024-02-23 12:35:20 +0000

    Animations triggering with Gameplay Abilities

--- Source/GAS_Test/MyAttributeSet.cpp
+++ Source/GAS_Test/MyAttributeSet.cpp
@@ -44,63 +44,7 @@ void UMyAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallback
 {
 	Super::PostGameplayEffectExecute(Data);
 
-	/*FGameplayEffectContextHandle Context = Data.EffectSpec.GetContext();
-	UAbilitySystemComponent* Source = Context.GetOriginalInstigatorAbilitySystemComponent();
-	const FGameplayTagContainer& SourceTags = *Data.EffectSpec.CapturedSourceTags.GetAggregatedTags();
-	FGameplayTagContainer SpecAssetTags;
-	Data.EffectSpec.GetAllAssetTags(SpecAssetTags);
 
-	// Get target actor (owner)
-	AActor* TargetActor = nullptr;
-	AController* TargetController = nullptr;
-	AGAS_TestCharacter* TargetCharacter = nullptr;
-	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
-	{
-		TargetActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
-		TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
-		TargetCharacter = Cast<AGAS_TestCharacter>(TargetActor);
-	}
-
-	// Get the source Actor
-	AActor* SourceActor = nullptr;
-	AController* SourceController = nullptr;
-	AGAS_TestCharacter* SourceCharacter = nullptr;
-	if (Source && Source->AbilityActorInfo.IsValid() && Source->AbilityActorInfo->AvatarActor.IsValid())
-	{
-		SourceActor = Source->AbilityActorInfo->AvatarActor.Get();
-		SourceController = Source->AbilityActorInfo->PlayerController.Get();
-		if (SourceController == nullptr && SourceActor != nullptr)
-		{
-			if (APawn* Pawn = Cast<APawn>(SourceActor))
-			{
-				SourceController = Pawn->GetController();
-			}
-		}
-
-		// Use Controller to find the source pawn
-		if (SourceController)
-		{
-			SourceCharacter = Cast<AGAS_TestCharacter>(SourceController->GetPawn());
-		}
-		else {
-			SourceCharacter = Cast<AGAS_TestCharacter>(SourceActor);
-		}
-
-		// Set the causer actor based on context if it's set
-		if (Context.GetEffectCauser())
-		{
-			SourceActor = Context.GetEffectCauser();
-		}
-	}*/
-
-	/*if (Data.EvaluatedData.Attribute == GetHealthAttribute())
-	{
-		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
-	}
-	else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
-	{
-		SetStamina(FMath::Clamp(GetStamina(), 0.0f, GetMaxStamina()));
-	}*/
 	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
 	{
 		// store local copy of the amaount of damage done and clear the damage attribute
